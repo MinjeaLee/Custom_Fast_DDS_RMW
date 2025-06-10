56d7beadbcca98ce6a68a8ec69149f752c6b7290
MinjaeLee <gabe0529@naver.com>
2025-06-10 02:17:41 +0000

    Mod : 로그 저장 폴더 변경경

--- rmw_fastrtps_shared_cpp/include/rmw_fastrtps_shared_cpp/custom_subscriber_info.hpp
+++ rmw_fastrtps_shared_cpp/include/rmw_fastrtps_shared_cpp/custom_subscriber_info.hpp
@@ -72,7 +72,7 @@ public:
       auto now = std::chrono::system_clock::now();
       auto time_t = std::chrono::system_clock::to_time_t(now);
       std::stringstream ss;
-      ss << "/tmp/fast_listener.log";
+      ss << "/tmp/DRSFuzz/fast_listener.log";
       log_path_ = ss.str();
     }
     
